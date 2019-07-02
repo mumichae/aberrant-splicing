e568b1072bd229379f4409c7a757d857a79b8922
Salazar Daniela Soraya Andrade <salazar@in.tum.de>
2019-07-02 16:04:06 +0200

    Update 04_fit_hyperparameters_FraseR.R

--- Scripts/FraseR/04_fit_hyperparameters_FraseR.R
+++ Scripts/FraseR/04_fit_hyperparameters_FraseR.R
@@ -28,7 +28,7 @@ source("./src/r/config.R")
 
 #+ input
 dataset    <- snakemake@wildcards$dataset
-workingDir <- file.path(snakemake@config$ROOT, snakemake@config$DATASET_NAME, "processed_data", "datasets")
+workingDir <- file.path(snakemake@config$ROOT, snakemake@config$DATASET_NAME, "processed_data", "aberrant_splicing", "datasets")
 bpWorkers  <- min(bpworkers(), as.integer(snakemake@params[[1]]$workers))
 bpThreads  <- min(bpworkers(), as.integer(snakemake@params[[1]]$threads))
 bpProgress <- snakemake@params[[1]]$progress
