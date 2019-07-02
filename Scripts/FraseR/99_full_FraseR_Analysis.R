bdc5b5cfe993029abad30668c105caf3211d92cd
Salazar Daniela Soraya Andrade <salazar@in.tum.de>
2019-07-02 16:02:14 +0200

    Update 99_full_FraseR_Analysis.R

--- Scripts/FraseR/99_full_FraseR_Analysis.R
+++ Scripts/FraseR/99_full_FraseR_Analysis.R
@@ -4,7 +4,7 @@
 #' author: Christian Mertes
 #' wb:
 #'  input:
-#'   - resultTable: '`sm expand(parser.getProcDataDir()+ "/results/{dataset}_results.tsv", dataset=config["DATASETS"])`'
+#'   - resultTable: '`sm expand(parser.getProcDataDir()+ "/aberrant_splicing/results/{dataset}_results.tsv", dataset=config["DATASETS"])`'
 #' output:
 #'  html_document
 #'---
