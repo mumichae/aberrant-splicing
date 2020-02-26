d39f64b19031efeb501974a09cb6660d054827e6
Vicente <yepez@in.tum.de>
2020-02-26 17:39:19 +0100

    fraser results updated

--- Scripts/FRASER/07_extract_results_FraseR.R
+++ Scripts/FRASER/07_extract_results_FraseR.R
@@ -11,8 +11,10 @@
 #'                 "/aberrant_splicing/datasets/savedObjects/{dataset}/" +
 #'                 "pajdBetaBinomial_psiSite.h5"`'
 #'  output:
-#'   - resultTable: '`sm parser.getProcDataDir() + 
-#'                       "/aberrant_splicing/results/{dataset}_results.tsv"`'
+#'   - resultTableJunc: '`sm parser.getProcDataDir() + 
+#'                          "/aberrant_splicing/results/{dataset}_results_per_junction.tsv"`'
+#'   - resultTableGene: '`sm parser.getProcDataDir() + 
+#'                          "/aberrant_splicing/results/{dataset}_results.tsv"`'
 #'  type: script
 #'---
 
@@ -30,37 +32,41 @@ register(MulticoreParam(snakemake@threads))
 
 params <- snakemake@config$aberrantSplicing
 
-
-# Load data
+# Load data and annotate ranges with gene names
 fds <- loadFraseRDataSet(dir=workingDir, name=dataset)
-
-# Annotate ranges
 fds <- annotateRanges(fds)
 
-# Extract results
-resgr <- results(fds,
+# Extract results per junction
+res_junc <- results(fds,
                  padjCutoff=params$padjCutoff, 
                  zScoreCutoff=params$zScoreCutoff,
                  deltaPsiCutoff=params$deltaPsiCutoff)
-res   <- as.data.table(resgr)
+res_junc_dt   <- as.data.table(res_junc)
 saveFraseRDataSet(fds)
 
-# Add features
-if(nrow(res) > 0){
-  # number of samples per gene and variant  
-  res[padjust <= params$padjCutoff,
-    numSamplesPerGene := length(unique(sampleID)), by=hgncSymbol]
-  res[padjust <= params$padjCutoff, 
-    numEventsPerGene :=.N, by="hgncSymbol,sampleID"]
-  res[padjust <= params$padjCutoff, 
-    numSamplesPerJunc:=length(unique(sampleID)), by="seqnames,start,end"]
+# Add features 
+if(nrow(res_junc_dt) > 0){
   
-  # add colData at the end
-  res <- merge(res, as.data.table(colData(fds)), by="sampleID")
+  # number of samples per gene and variant  
+  res_junc_dt[, numSamplesPerGene := uniqueN(sampleID), by = hgncSymbol]
+  res_junc_dt[, numEventsPerGene := .N, by = "hgncSymbol,sampleID"]
+  res_junc_dt[, numSamplesPerJunc := uniqueN(sampleID), by = "seqnames,start,end"]
+  
+  # add colData to the results
+  res_junc_dt <- merge(res_junc_dt, as.data.table(colData(fds)), by = "sampleID")
+  res_junc_dt[, bamFile := NULL]
 } else{
-  warning(paste0("The aberrant splicing pipeline gave 0 results for the ", dataset, " dataset."))
+  warning("The aberrant splicing pipeline gave 0 results for the ", dataset, " dataset.")
 }
 
+# Aggregate results by gene
+if(length(res_junc) > 0){
+  res_genes_dt <- resultsByGenes(res_junc) %>% as.data.table
+  res_genes_dt <- merge(res_genes_dt, as.data.table(colData(fds)), by = "sampleID")
+  res_genes_dt[, bamFile := NULL]
+} else res_genes_dt <- data.table()
+
 # Results
-write_tsv(res, file=snakemake@output$resultTable)
+write_tsv(res_junc_dt, file=snakemake@output$resultTableJunc)
+write_tsv(res_genes_dt, file=snakemake@output$resultTableGene)
 
