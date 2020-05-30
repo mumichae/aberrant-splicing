90560c95a21ffa02c1c08e241b42d0c8018cd1ae
Michaela Mueller <mi.mueller@tum.de>
2020-05-30 10:02:48 +0200

    separated count export to separate script

---
+++ Scripts/Counting/exportCounts.R
@@ -0,0 +1,48 @@
+#'---
+#' title: Collect all counts to FRASER Object
+#' author: Michaela Müller
+#' wb:
+#'  params:
+#'   - tmpdir: '`sm drop.getMethodPath(METHOD, "tmp_dir")`'
+#'   - workingDir: '`sm parser.getProcDataDir() + "/aberrant_splicing/datasets"`'
+#'  input:
+#'   - gRangesSplitCounts: '`sm parser.getProcDataDir() + 
+#'                          "/aberrant_splicing/datasets/cache/raw-{dataset}/gRanges_splitCounts.rds"`'
+#'   - spliceSites: '`sm parser.getProcDataDir() + 
+#'                   "/aberrant_splicing/datasets/cache/raw-{dataset}/spliceSites_splitCounts.rds"`'
+#'   - counting_done: '`sm parser.getProcDataDir() + 
+#'                "/aberrant_splicing/datasets/savedObjects/raw-{dataset}/counting.done" `'
+#'  output:
+#'    - split_counts: '`sm parser.getProcResultsDir() + "/exported_counts/{dataset}/"
+#'                + "splitCounts_{annotation}.tsv.gz"`'
+#'    - nonsplit_counts: '`sm parser.getProcResultsDir() + "/exported_counts/{dataset}/"
+#'                + "spliceSiteOverlapCounts_{annotation}.tsv.gz"`'
+#'  type: script
+#'---
+saveRDS(snakemake, file.path(snakemake@params$tmpdir, "export.snakemake"))
+# snakemake <- readRDS(".drop/tmp/AS/export.snakemake")
+
+source("Scripts/_helpers/config.R")
+
+dataset    <- snakemake@wildcards$dataset
+workingDir <- snakemake@params$workingDir
+
+
+# Read FRASER object
+fds <- loadFraserDataSet(dir=workingDir, name=paste0("raw-", dataset))
+splitCounts_gRanges <- readRDS(snakemake@input$gRangesSplitCounts)
+spliceSiteCoords <- readRDS(snakemake@input$spliceSites)
+
+# obtain the split counts
+splitCounts <- assays(fds)[['rawCountsJ']]
+gr_dt <- as.data.table(splitCounts_gRanges)[, c(1:3,5)]
+splitCounts <- cbind(gr_dt, as.matrix(splitCounts))
+fwrite(splitCounts, file = snakemake@output$split_counts,
+       quote = FALSE, row.names = FALSE, sep = '\t', compress = 'gzip')
+  
+# obtain the non split counts
+nonSplitCounts <- assays(fds)[['rawCountsSS']]
+grns_dt <- as.data.table(spliceSiteCoords)[, c(1:3,5)]
+nonSplitCounts <- cbind(grns_dt, as.matrix(nonSplitCounts))
+fwrite(nonSplitCounts, file = snakemake@output$nonsplit_counts,
+       quote = FALSE, row.names = FALSE, sep = '\t', compress = 'gzip')
